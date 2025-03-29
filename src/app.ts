1e6c3cb304599dd32a9a044d491130279f93abd7
Vighnesh Pawar <vighneshpawar004@gmail.com>
2025-03-29 11:18:54 +0530

    add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,32 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import logger from './config/logger'
+import { HttpError } from 'http-errors'
 
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Welcome to auth service')
+    // for async functions it not handle  using throw err
+    // for that we use next
+    // normally we use next() to give control to next function \
+    // but if we use next(args) then it considered as error and passed to global handleer
+})
+
+//global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message)
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    })
 })
 
 export default app
